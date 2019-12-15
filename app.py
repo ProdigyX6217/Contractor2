5fb988dee9299ff4e02426985221ad13edc140b7
ProdigyX6217 <dominic.holder.15@cityas.org>
2019-12-14 19:00:41 -0800

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from Flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Return homepage."""
+    
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
