94f1ce77c44d0133b4567b89c6b04669247e0d1e
ProdigyX6217 <dominic.holder.15@cityas.org>
2019-12-14 21:39:36 -0800

    Users can create albums

--- app.py
+++ app.py
@@ -15,16 +15,36 @@ app = Flask(__name__)
 
 # OUR ARRAY OF ALBUMS
 albums = [
-    { 'title': 'Is This It', 'genre': 'Indie' },
-    { 'title': 'Channel Orange', 'genre': 'R&B' },
-    { 'title': 'The Blueprint', 'genre': 'Hip-Hop' },
-    { 'title': 'Dark Side of the Moon', 'genre': 'Rock' }
+    { 'title': 'Is This It', 'genre': 'Indie', 'artist': 'The Strokes'},
+    { 'title': 'Channel Orange', 'genre': 'R&B', 'artist': 'Frank Ocean' },
+    { 'title': 'The Blueprint', 'genre': 'Hip-Hop', 'artist': 'JAY-Z' },
+    { 'title': 'Dark Side of the Moon', 'genre': 'Rock', 'artist': 'Pink Floyd' }
 ]
 
 @app.route('/')
 def albums_index():
     """Show all albums."""
     return render_template('albums_index.html', albums=albums)
+
+@app.route('/albums/new')
+def albums_new():
+    """Create a new album."""
+    return render_template('albums_new.html')
+
+@app.route('/albums', methods=['POST'])
+def albums_submit():
+    """Submit a new album."""
+    album = {
+        'title': request.form.get('title'),
+        'genre': request.form.get('genre'),
+        'artist': request.form.get('artist'),
+        'url': request.form.get('url')
+    }
+    albums.insert_one(album)
+    return redirect(url_for('albums_index'))
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
