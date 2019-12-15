124ce126e7e61d5a299df2491322cc6e21447d26
ProdigyX6217 <dominic.holder.15@cityas.org>
2019-12-14 19:29:03 -0800

    Users can see all albums

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from Flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def index():
+#     """Return homepage."""
+#     return render_template('index.html')
+
+# OUR MOCK ARRAY OF PROJECTS
+albums = [
+    { 'title': 'Is This It', 'genre': 'Indie' },
+    { 'title': 'Channel Orange', 'genre': 'R&B' },
+    { 'title': 'The Blueprint', 'genre': 'Hip-Hop' },
+    { 'title': 'Dark Side of the Moon', 'genre': 'Rock' }
+]
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    
-    return render_template('index.html')
+def albums_index():
+    """Show all albums."""
+    return render_template('albums_index.html', albums=albums)
 
 if __name__ == "__main__":
     app.run(debug=True)
