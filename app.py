d17f40af91b7118c7912d2bc170688541ad3f6d7
ProdigyX6217 <dominic.holder.15@cityas.org>
2019-12-16 02:07:59 -0800

    Users can edit, update, and destroy playlists

--- app.py
+++ app.py
@@ -9,62 +9,75 @@ albums = db.albums
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def index():
-#     """Return homepage."""
-#     return render_template('index.html')
-
 # OUR ARRAY OF ALBUMS
 albums = [
-    { 'title': 'Is This It', 'genre': 'Indie', 'artist': 'The Strokes'},
-    { 'title': 'Channel Orange', 'genre': 'R&B', 'artist': 'Frank Ocean' },
-    { 'title': 'The Blueprint', 'genre': 'Hip-Hop', 'artist': 'JAY-Z' },
-    { 'title': 'Dark Side of the Moon', 'genre': 'Rock', 'artist': 'Pink Floyd' },
-    { 'title': 'Viva la Vida or Death and All His Friends', 'genre': 'Pop', 'artist': 'Coldplay' },
+    { 'title': 'Is This It', 'genre': 'Indie', 'artist': 'The Strokes', 'url': 'https://bit.ly/36EoFAQ'},
+    { 'title': 'Channel Orange', 'genre': 'R&B', 'artist': 'Frank Ocean', 'url': 'https://bit.ly/2LZ7S3z' },
+    { 'title': 'The Blueprint', 'genre': 'Hip-Hop', 'artist': 'JAY-Z', 'url': 'https://bit.ly/36Bc5lW' },
+    { 'title': 'Dark Side of the Moon', 'genre': 'Rock', 'artist': 'Pink Floyd', 'url': 'https://bit.ly/2sqLR75' },
+    { 'title': 'Viva la Vida or Death and All His Friends', 'genre': 'Pop', 'artist': 'Coldplay', 'url': 'https://bit.ly/38G92KZ' },
 
 ]
+
+def image_url_creator(id_lst):
+    images = []
+    for img_id in id_lst:
+        image = 'url' + img_id
+        images.append(image)
+    return images
+
 
 @app.route('/')
 def albums_index():
     """Show all albums."""
     return render_template('albums_index.html', albums=albums)
 
+
 @app.route('/albums/new')
 def albums_new():
     """Create a new album."""
-    return render_template('albums_new.html')
+    return render_template('albums_new.html', title ='New Album')
+
 
 @app.route('/albums', methods=['POST'])
 def albums_submit():
     """Submit a new album."""
+
+    image_ids = request.form.get('image_ids').split()
+    images = image_url_creator(image_ids)
+
     album = {
         'title': request.form.get('title'),
         'genre': request.form.get('genre'),
         'artist': request.form.get('artist'),
-        'url': request.form.get('url')
+        'url': 'url',
+        'image_ids': 'image_ids'
     }
-    albums.insert_one(album)
+    album_id = albums.insert_one(album).inserted_id
     return redirect(url_for('albums_index'))
+
 
 @app.route('/albums/<album_id>')
 def albums_show(album_id):
     """Show a single album."""
     album = albums.find_one({'_id': ObjectId(album_id)})
-    album_comments = comments.find({'album_id': ObjectId(album_id)})
+    # album_comments = comments.find({'album_id': ObjectId(album_id)})
     return render_template('albums_show.html', album=album)
+
 
 @app.route('/albums/<album_id>', methods=['POST'])
 def albums_update(album_id):
     """Submit an edited album."""
-    album_ids = request.form.get('video_ids').split()
-    albums = video_url_creator(video_ids)
+    image_ids = request.form.get('image_ids').split()
+    albums = image_url_creator(image_ids)
 
     # create our updated album
     updated_album = {
         'title': request.form.get('title'),
         'genre': request.form.get('genre'),
         'artist': artist,
-        'url': url
+        'url': url,
+        'image_ids': image_ids
     }
 
     # set the former album to the new one we just updated/edited
@@ -74,8 +87,9 @@ def albums_update(album_id):
     # take us back to the album's show page
     return redirect(url_for('albums_show', album_id=album_id))
 
+
 @app.route('/albums/<album_id>/edit')
- def albums_edit(album_id):
+def albums_edit(album_id):
      """Show the edit form for a album."""
      album = albums.find_one({'_id': ObjectId(album_id)})
      # Add the title parameter here
